6b3306f1def30af6094cba1327e149e66f41df72
rohan472000 <anand00rohan@gmail.com>
2023-12-29 23:14:39 +0530

    learned case and loops as expression

--- src/main/scala/test/first.scala
+++ src/main/scala/test/first.scala
@@ -45,5 +45,43 @@ object third{
     for (i <- 1 until 5) println("i : "+i) // use of until as normal english word without "."
     // for loop for two variables
     for (i <- 1 to 5; j <- 5 to 8) println(s"i is : $i and j is : $j")
+
+    // list iterations by for loop
+    val lst = List(2,3,4,5,65,34)
+    for (i <- lst; if i < 6) print(i) // putting condition while iterating through if statement
+    println()
+
+    // using for loop as expression
+    var list = for {i <- lst; if i < 8} yield{
+      i * i
+    }
+    print(list)
+  }
+}
+
+object fourth{
+  def main(args: Array[String]): Unit = {
+    val age = 10
+
+    age match{
+      case 12 => println(age)
+      case 10 => println(age)
+      case _ => println(19) //  default statement should be at last
+    }
+
+    // saving result in a variable
+    val res = age match {
+      case 12 => age
+      case 10 => age
+      case _ => 19 //  default statement should be at last
+    }
+    println("res is : "+res)
+
+    // other way to use case statement
+    val i = 7
+    i match {
+      case 1|3|5|7|9 => println("odd")
+      case 2|4|6|8|10 => println("even")
+    }
   }
 }
