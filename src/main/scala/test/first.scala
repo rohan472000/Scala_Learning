0eed0f7fed32b177e631dc0d730a109acc57bd02
rohan472000 <anand00rohan@gmail.com>
2023-12-29 22:35:59 +0530

    learned if else

--- src/main/scala/test/first.scala
+++ src/main/scala/test/first.scala
@@ -13,8 +13,17 @@ object first {
 
 object second{
   def main(args: Array[String]): Unit = {
-    val a = 10
-    if (a == 10) print(true)
-    else print(false)
+    val (a,y) = (10,20) // another way to initialize constant variables
+
+    if (a == 10 && y==20) println(true) // && is AND operator and || is OR operator
+    else println(false)
+
+    var res = ""
+    if (a==10) res="a==10"
+    else  res="a!=10"
+
+    println("res is : "+res)
+    val res1 = if(a==10) "a==10" else "a!=10"
+    println("res1 is : "+res1)
   }
 }
